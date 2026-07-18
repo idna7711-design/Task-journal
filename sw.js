a02006b12f0666ddbddd0871b01dd04689f26437
idna7711-design <idna7711@gmail.com>
2026-07-19 08:00:05 +0900

    タスク格納ボックスを排他的に開閉する (#50)

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * 注意: タスクや設定(APIキー等)は IndexedDB に保存されており、このキャッシュとは無関係。
  *       キャッシュを消してもユーザーデータは消えない。
  */
-const CACHE = 'taskjournal-cache-v8';
+const CACHE = 'taskjournal-cache-v9';
 const CORE_ASSETS = [
     './',
     './index.html',
